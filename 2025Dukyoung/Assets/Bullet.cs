8d1a06065df2fd2e442d4b35da31c39f5b30a486
Im.GW <50003828+lim12749@users.noreply.github.com>
2025-05-26 17:42:38 +0900

    몬스터 샘플 죽는거 만듬
    Todo : 애니메이션연결해야함

--- 2025Dukyoung/Assets/Bullet.cs
+++ 2025Dukyoung/Assets/Bullet.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float lifeTime = 3f; //시간되면 삭제
-
+    public float damage = 10f;
     void Start()
     {
         Destroy(gameObject, lifeTime); //3초 뒤에 삭제
     }
     void OnCollisionEnter(Collision other)
     {
+        //최신 TryGet으로 out으로 매개변수로 전달
+        if (other.collider.TryGetComponent<Monster>(out var monster))
+        {
+            monster.TakeDamage(damage);
+        }
         Destroy(gameObject); //충돌하면 삭제
         //여기에 충돌 피격 효과 이펙트도 추가
+        
     }
 }
